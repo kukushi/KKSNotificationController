d279fc9013ea9c290f182fa76ec8752901576cea
kukushi <nekop@outlook.com>
2014-11-12 14:39:10 +0800

    Initial Commit

---
+++ KKSNotificationController/KKSNotificationController.h
@@ -0,0 +1,13 @@
+//
+//  KKSNotificationController.h
+//  KKSNotificationController
+//
+//  Created by kukushi on 11/12/14.
+//  Copyright (c) 2014 Xing He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKSNotificationController : NSObject
+
+@end
---
+++ KKSNotificationController/KKSNotificationController.m
@@ -0,0 +1,13 @@
+//
+//  KKSNotificationController.m
+//  KKSNotificationController
+//
+//  Created by kukushi on 11/12/14.
+//  Copyright (c) 2014 Xing He. All rights reserved.
+//
+
+#import "KKSNotificationController.h"
+
+@implementation KKSNotificationController
+
+@end
